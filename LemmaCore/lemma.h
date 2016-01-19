b2b0c802ccd09ce44b28bcb97627e0674589c43a
Trevor Irons <Trevor.Irons@lemmasoftware.org>
2016-01-19 10:36:20 -0700

    Testing CMake build, just getting going.

---
+++ LemmaCore/lemma.h
@@ -0,0 +1,21 @@
+/* This file is part of Lemma, a geophysical modelling and inversion API.
+ * More information is available at http://lemmasoftware.org
+ */
+
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+/**
+ * @file
+ * @date      01/16/2016 04:40:33 PM
+ * @version   $Id$
+ * @author    Trevor Irons (ti)
+ * @email     tirons@egi.utah.edu
+ * @copyright Copyright (c) 2016, University of Utah
+ * @copyright Copyright (c) 2016, Trevor Irons & Lemma Software, LLC
+ */
+
+float thingy();
+
