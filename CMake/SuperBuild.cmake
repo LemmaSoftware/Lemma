a81921d01e210c076696fe92c0eee23df0bb90f8
Trevor Irons <tirons@egi.utah.edu>
2018-02-08 13:46:10 -0700

    fix for CxxTest in superbuild

--- CMake/SuperBuild.cmake
+++ CMake/SuperBuild.cmake
@@ -13,8 +13,11 @@ else()
         #CONFIGURE_COMMAND ""
         #BUILD_COMMAND ""
     )
-    include_directories ("${CMAKE_INSTALL_PREFIX}/include/eigen3/")
-    set (Eigen3_DIR  "${CMAKE_INSTALL_PREFIX}/include/eigen3" CACHE  PATH "" FORCE )
+    
+    # Are these necessary?
+    #include_directories ("${CMAKE_INSTALL_PREFIX}/include/eigen3/")
+    #set (Eigen3_DIR  "${CMAKE_INSTALL_PREFIX}/include/eigen3" CACHE  PATH "" FORCE )
+    
     #find_package (Eigen3 3.3 NO_MODULE REQUIRED)
     #if (eigen3_FOUND)
     #    message( STATUS "Eigen was found!!!!!!!!!!!!!! ${eigen_DIR}" )
@@ -42,34 +45,36 @@ else()
                    -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TOOLCHAIN_FILE} 
     	           -DYAML_CPP_BUILD_TESTS=OFF
 	)	
-	include_directories ("${CMAKE_INSTALL_PREFIX}/include/yaml-cpp")
-	add_library(yaml-my-cpp SHARED IMPORTED)
+    # is this necessary?
+	#include_directories ("${CMAKE_INSTALL_PREFIX}/include/yaml-cpp")
 endif()
 
 if (LEMMA_ENABLE_TESTING)
-if (CXXTEST_FOUND)
-    #message( STATUS "CXXTEST FOUND" )
-else()
-	#message( STATUS "CxxTest will be built and installed along with Lemma, override if desired in GUI."  )
-	ExternalProject_Add(CxxTest
-		GIT_REPOSITORY "https://github.com/CxxTest/cxxtest.git"
-		GIT_TAG "master"
-		UPDATE_COMMAND ""
-		PATCH_COMMAND ""
-		CONFIGURE_COMMAND ""
-		BUILD_COMMAND ""
-		INSTALL_COMMAND ""
-    	PREFIX ${CMAKE_CURRENT_BINARY_DIR}/external/CxxTest
-    	CMAKE_ARGS -DCMAKE_INSTALL_PREFIX:PATH=${CMAKE_INSTALL_PREFIX} /
-                   -DBUILD_SHARED_LIBS=${BUILD_SHARED_LIBS} /
-                   -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}
-	)
-	# The values "CACHE PATH "" FORCE" makes the GUI show the values of the local CxxTest, but this can cause issues 
-	# with users wanting to specify their own path, its not recommended  
-	#set (CXXTEST_INCLUDE_DIR  "${CMAKE_CURRENT_BINARY_DIR}/external/CxxTest/src/CxxTest/") # CACHE  PATH "" FORCE )
-	#set (CXXTEST_PYTHON_TESTGEN_EXECUTABLE "${CMAKE_CURRENT_BINARY_DIR}/external/CxxTest/src/CxxTest/bin/cxxtestgen") # CACHE PATH "" FORCE )
-	#find_package(CxxTest REQUIRED)
-endif()
+    if (CXXTEST_FOUND)
+        message( STATUS "CxxTest was found" )
+    else()
+	    message( STATUS "CxxTest WAS NOT FOUND") 
+	    ExternalProject_Add(CxxTest
+		    GIT_REPOSITORY "https://github.com/CxxTest/cxxtest.git"
+		    GIT_TAG "master"
+		    UPDATE_COMMAND ""
+		    PATCH_COMMAND ""
+		    CONFIGURE_COMMAND ""
+		    BUILD_COMMAND ""
+		    INSTALL_COMMAND ""
+    	    PREFIX ${CMAKE_CURRENT_BINARY_DIR}/external/CxxTest
+    	    CMAKE_ARGS -DCMAKE_INSTALL_PREFIX:PATH=${CMAKE_INSTALL_PREFIX} /
+                       -DBUILD_SHARED_LIBS=${BUILD_SHARED_LIBS} /
+                       -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}
+	    )
+        #enable_testing()
+	    # The values "CACHE PATH "" FORCE" makes the GUI show the values of the local CxxTest, but this can cause issues 
+	    # with users wanting to specify their own path. However, the SuperBuild needs this, in theory you are only here 
+        # because a usable CXXTEST was not already found on the machine, so likely this is sane to do  
+	    set (CXXTEST_INCLUDE_DIR  "${CMAKE_CURRENT_BINARY_DIR}/external/CxxTest/src/CxxTest/" CACHE  PATH "" FORCE )
+	    set (CXXTEST_PYTHON_TESTGEN_EXECUTABLE "${CMAKE_CURRENT_BINARY_DIR}/external/CxxTest/src/CxxTest/bin/cxxtestgen" CACHE PATH "" FORCE )
+	    find_package(CxxTest REQUIRED)
+    endif()
 endif()
 
 # tinyxml2, this library is used for XML IO 
