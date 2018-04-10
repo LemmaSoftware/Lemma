4f7a8512abe60b0c7e65b6646d1686ad3133f718
Trevor Irons <tirons@egi.utah.edu>
2018-04-10 16:17:39 -0600

    Improved cross compiling using GNU tools for WIN64

--- CMake/Mingw-Cross.cmake
+++ CMake/Mingw-Cross.cmake
@@ -9,7 +9,8 @@ set(CMAKE_SYSTEM_NAME Windows)
 set(CMAKE_SYSTEM Windows)
 set(TOOLCHAIN_PREFIX x86_64-w64-mingw32)
 
-SET(CMAKE_CROSSCOMPILING 1)
+option (CMAKE_CROSSCOMPILING "Target different arch than you are on" ON)
+SET(CMAKE_CROSSCOMPILING ON)
 
 # cross compilers to use for C and C++
 set(CMAKE_C_COMPILER ${TOOLCHAIN_PREFIX}-gcc)
@@ -23,10 +24,17 @@ set(CMAKE_FIND_ROOT_PATH /usr/${TOOLCHAIN_PREFIX})
 # modify default behavior of FIND_XXX() commands to
 # search for headers/libs in the target environment and
 # search for programs in the build host environment
-set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
-set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
-set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
-set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
+set( CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER )
+set( CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY )
+set( CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY )
+set( CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY )
 
-set(CMAKE_SHARED_LIBRARY_LINK_C_FLAGS "")
-set(CMAKE_SHARED_LIBRARY_LINK_CXX_FLAGS "")
+set( CMAKE_SHARED_LIBRARY_LINK_C_FLAGS "" )
+set( CMAKE_SHARED_LIBRARY_LINK_CXX_FLAGS "" )
+
+#set_target_properties(icarus PROPERTIES LINK_SEARCH_START_STATIC 1)
+#set_target_properties(icarus PROPERTIES LINK_SEARCH_END_STATIC 1)
+#set(CMAKE_FIND_LIBRARY_SUFFIXES ".a")
+#Static Libs
+#Set Linker flags
+set(CMAKE_EXE_LINKER_FLAGS "-static-libgcc -static-libstdc++")
