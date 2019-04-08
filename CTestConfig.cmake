f0e7ddc6e5a21a666a0d2f5f9299a0960944d60a
Trevor Irons <Trevor.Irons@lemmasoftware.org>
2019-04-08 16:25:45 -0600

    Fix for CTest in versions 3.14 and greater

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -2,8 +2,9 @@
 ## Then modify the CMakeLists.txt file in the root directory of your
 ## project to incorporate the testing dashboard.
 ## # The following are required to uses Dart and the Cdash dashboard
-##   ENABLE_TESTING()
-##   INCLUDE(CTest)
+ENABLE_TESTING()
+#INCLUDE(CTest)
+
 set(CTEST_PROJECT_NAME "Lemma")
 set(CTEST_NIGHTLY_START_TIME "01:00:00 UTC")
 
@@ -11,4 +12,5 @@ set(CTEST_DROP_METHOD "http")
 set(CTEST_DROP_SITE "my.cdash.org")
 set(CTEST_DROP_LOCATION "/submit.php?project=Lemma")
 set(CTEST_DROP_SITE_CDASH TRUE)
-
+# CTest 3.14 requires this
+set(CTEST_SUBMIT_URL "my.cdash.org/submit.php?project=Lemma")
